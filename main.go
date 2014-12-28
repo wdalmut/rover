f3efc3b940d3aa184351943c76bbfd52318d60cf
Walter Dal Mut <walter.dalmut@gmail.com>
2014-12-28 18:42:16 +0100

    Added twitter stream and configuration

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 
+	"github.com/darkhelmet/twitterstream"
 	"github.com/wdalmut/rover/sentiment"
+	"github.com/wdalmut/twitterstream/async"
 )
+
+type Config struct {
+	ConsumerKey    string
+	ConsumerSecret string
+	AccessToken    string
+	AccessSecret   string
+}
 
 func main() {
 	classifier := sentiment.NewRoverClassifierFromFiles("files/rt-polarity.pos", "files/rt-polarity.neg")
 
-	class := classifier.Classify("I am so happy")
-	fmt.Printf("Sentence is %v\n", class)
+	file, e := ioutil.ReadFile("./config.json")
+	if e != nil {
+		fmt.Printf("File error: %v\n", e)
+		os.Exit(1)
+	}
 
-	class = classifier.Classify("I am so unhappy")
-	fmt.Printf("Sentence is %v\n", class)
+	var config Config
+	json.Unmarshal(file, &config)
+
+	fmt.Printf("%v", config)
+
+	client := async.NewClient(
+		config.ConsumerKey,
+		config.ConsumerSecret,
+		config.AccessToken,
+		config.AccessSecret,
+	)
+
+	client.TrackAndServe("bieber", func(tweet *twitterstream.Tweet) {
+		class := classifier.Classify(tweet.Text)
+
+		fmt.Printf("Tweet: %s is %v\n\n", tweet.Text, class)
+	})
 }
